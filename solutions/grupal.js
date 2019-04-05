0bb13bbc947c309d047546ad0f16fea46f8939c6
Melvin Figueroa <melproducc@gmail.com>
2019-04-05 04:32:13 +0200

    grupal

---
+++ solutions/grupal.js
@@ -0,0 +1,5 @@
+// /////////// ------------------ ejercicio el uno
+// /////////// ------------------ ejercicio el dos
+// /////////// ------------------ ejercicio el tres
+// /////////// ------------------ ejercicio el cuatro
+// /////////// ------------------ ejercicio el cinco
