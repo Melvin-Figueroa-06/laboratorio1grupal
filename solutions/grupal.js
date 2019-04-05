bed668ac24e7ccd268a88c2fce02dd15922bf923
juan <juan_carlos_jctq@hotmail.com>
2019-04-05 09:46:00 -0400

    ejercicio 1

--- solutions/grupal.js
+++ solutions/grupal.js
@@ -1,4 +1,13 @@
 // /////////// ------------------ ejercicio el uno
+function sumar(valor)
+    {
+        var suma=0;
+        for(var i=0;i<valor.length;i++)
+        {
+            return suma+=parseInt(valor[i]);
+        }
+        module.exports = suma
+    }
 // /////////// ------------------ ejercicio el dos
 // /////////// ------------------ ejercicio el tres
 // /////////// ------------------ ejercicio el cuatro
