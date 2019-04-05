b59ac785f4713407ad45446e2e385504833ddada
juan <juan_carlos_jctq@hotmail.com>
2019-04-05 10:09:01 -0400

    Ejercicio 2 daniel

--- solutions/grupal.js
+++ solutions/grupal.js
@@ -9,6 +9,28 @@ function sumar(valor)
         module.exports = suma
     }
 // /////////// ------------------ ejercicio el dos
+
+function candies(n,m)
+   {
+       var n=n;
+       var m=m;
+       var dul=parseInt(m/n);
+       var sobra=m%n;
+       var total=n*dul;
+       document.write("Cantidad de niños: " + n + "<br>");
+       document.write("Cantidad de Dulces: " + m + "<br>");
+       document.write("Cantidad de dulces por niño: " + parseInt(dul) + "<br>");
+       document.write("Total dulces repartidos: " + total +"<br>");
+       if (sobra==0){
+       	document.write("No hay residuos");
+       }
+       else {
+       document.write("Dulces que sobran: " + sobra + " Y no se puede repartir");
+       }
+
+   }
+   candies(3,10);
+   
 // /////////// ------------------ ejercicio el tres
 // /////////// ------------------ ejercicio el cuatro
 // /////////// ------------------ ejercicio el cinco
